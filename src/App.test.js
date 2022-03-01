7c7fd7befa82db0e55f807c7872ef5024f0490b2
Gbenga Olufeyimi <gbengacodes@Gbengas-MacBook-Pro.local>
2022-03-01 11:17:41 +0100

    Inital commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Find element with role button and text "change to blue"
+  const buttonElement = screen.getByRole("button", {
+    name: /change to blue/i,
+  });
+
+  // Expect button background color to be red
+  expect(buttonElement).toHaveStyle({
+    backgroundColor: "red",
+  });
+});
+
+test("button turns blue when clicked", () => {
+  render(<App />);
+  // Find element with role button and text "change to blue"
+  const buttonElement = screen.getByRole("button", {
+    name: /change to blue/i,
+  });
+
+  // Click button and expect the background color to change to blue
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveStyle({
+    backgroundColor: "blue",
+  });
+
+  // Expect button text to change to red
+  expect(buttonElement.textContent).toBe("Change to red");
 });
