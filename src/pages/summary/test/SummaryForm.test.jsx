4e0fb86723d76c38073d8f688a355846fba2bc43
Gbenga Olufeyimi <gbengacodes@Gbengas-MacBook-Pro.local>
2022-03-02 08:09:21 +0100

    tested summary form component

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,7 +1,12 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
-describe("the functionalities fof summary form", () => {
+describe("the functionalities for summary form", () => {
   it("initial conditions", () => {
     render(<SummaryForm />);
     const buttonElement = screen.getByRole("button", {
@@ -10,9 +15,31 @@ describe("the functionalities fof summary form", () => {
     const checkboxElement = screen.getByRole("checkbox");
     expect(checkboxElement).not.toBeChecked();
     expect(buttonElement).toBeDisabled();
-    fireEvent.click(checkboxElement);
+    userEvent.click(checkboxElement);
     expect(buttonElement).toBeEnabled();
-    fireEvent.click(checkboxElement);
+    userEvent.click(checkboxElement);
     expect(buttonElement).toBeDisabled();
+  });
+
+  test("popover response to hover", async () => {
+    render(<SummaryForm />);
+    // popover starts out hidden
+    const nullPopoverElement = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopoverElement).toBeNull();
+    // popover appears when we hover
+    const termsText = screen.getByText(/terms and conditions/i);
+    userEvent.hover(termsText);
+    const popoverElement = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popoverElement).toBeInTheDocument();
+
+    // popover disappears when me don't hover
+    userEvent.unhover(termsText);
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText(/no ice cream will actually be delivered/i)
+    );
   });
 });
